0a022d2c89d0dc83d306e1b97a5a7c99c1cb8177
bhavana13-cmd <kakkirenibhavana@gmail.com>
2025-12-26 14:08:03 +0530

    Add CRC16-CCITT implementation

---
+++ firmware/crc16_ccitt.cpp
@@ -0,0 +1,12 @@
+#include "crc16_ccitt.h"
+
+uint16_t crc16_ccitt(const uint8_t *data, uint16_t len) {
+    uint16_t crc = 0xFFFF;
+    for (uint16_t i = 0; i < len; i++) {
+        crc ^= (uint16_t)data[i] << 8;
+        for (uint8_t j = 0; j < 8; j++) {
+            crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : (crc << 1);
+        }
+    }
+    return crc;
+}
